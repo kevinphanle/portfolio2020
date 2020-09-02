dc11042a4ddff5ea1d767a51b21c27566391f019
Kevin Le <kevinphanle11@gmail.com>
2020-09-01 19:39:12 -0700

    Updates

---
+++ precache-manifest.d84f66313edd8284d23a3ebbb769dad5.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89803840e4c4717487b6fbcdd74a0ccb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c9a05d7cb7724c86cbb",
+    "url": "/static/css/2.557d7ed1.chunk.css"
+  },
+  {
+    "revision": "b33085d0ab0b0258561a",
+    "url": "/static/css/main.2252ae80.chunk.css"
+  },
+  {
+    "revision": "2c9a05d7cb7724c86cbb",
+    "url": "/static/js/2.31167814.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.31167814.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b33085d0ab0b0258561a",
+    "url": "/static/js/main.4f917abc.chunk.js"
+  },
+  {
+    "revision": "31c4208678f17cfcd052",
+    "url": "/static/js/runtime-main.702ae503.js"
+  },
+  {
+    "revision": "d2f141665d4902728e8e9aa21be570f7",
+    "url": "/static/media/Kevin_Le_Resume.d2f14166.pdf"
+  },
+  {
+    "revision": "20b61b801441a1fc22d369130ddb2020",
+    "url": "/static/media/icomoon.20b61b80.svg"
+  },
+  {
+    "revision": "50f6a412fe415f3df39ff041a3b9ca5f",
+    "url": "/static/media/icomoon.50f6a412.ttf"
+  },
+  {
+    "revision": "adf75a7c64e022e647ad0c3046c21c04",
+    "url": "/static/media/icomoon.adf75a7c.eot"
+  },
+  {
+    "revision": "ba3c0c9d2cd6d28be294f904d91b2a58",
+    "url": "/static/media/icomoon.ba3c0c9d.woff"
+  },
+  {
+    "revision": "e3f2fad3347e5c136354792bc55ef730",
+    "url": "/static/media/self.e3f2fad3.jpg"
+  },
+  {
+    "revision": "fa83a6d21f224dfdf232c62c9afc047f",
+    "url": "/static/media/sun-self.fa83a6d2.jpg"
+  }
+]);
